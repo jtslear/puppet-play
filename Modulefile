ab723f3cdbbf0db848dfa5d46540053af16c877a
John T Skarbek <jtslear@gmail.com>
2013-08-17 16:17:19 -0400

    Initial import

---
+++ Modulefile
@@ -0,0 +1,15 @@
+name    'jtslear-play'
+version '0.0.1'
+source 'UNKNOWN'
+author 'jtslear'
+license 'Apache License, Version 2.0'
+summary 'Deploy the play server'
+description 'Deploy version 3 of play allowing some awesome music'
+project_page 'https://github.com/jtslear/puppet-play'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
+dependency 'puppetlabs/vcsrepo', '>=0.1.2'
+dependency 'alup/rbenv', '>=1.2.0'
+dependency 'puppetlabs/mysql', '>=0.9.0'
+
---
+++ manifests/init.pp
@@ -0,0 +1,112 @@
+# == Class: play
+#
+# Full description of class play here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if it
+#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should not be used in preference to class parameters  as of
+#   Puppet 2.6.)
+#
+# === Examples
+#
+#  class { play:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2013 Your name here, unless otherwise noted.
+#
+class play (
+  $mysql_root_password = 'foo',
+  $music_directory = '/opt/music',
+) {
+
+  class { "mysql::server":
+      config_hash => { 
+        "root_password" => "$mysql_root_password",
+      }
+  }
+  exec { "update":
+    command => "apt-get update",
+     path => "/bin:/usr/bin",
+  }
+
+  package { [
+      "git",
+    ]:
+    ensure  => "present",
+    require => Exec["update"],
+  }
+
+  define play_user {
+    user { "${name}":
+      ensure => "present",
+      comment => "Muzica User",
+      managehome => true,
+    }
+
+    file { "/opt/music":
+      ensure => "directory",
+      owner => "${name}",
+      group => "${name}",
+      mode => "0777",
+    }
+
+    vcsrepo { "/home/${name}/play":
+      ensure   => "latest",
+      user => "${name}",
+      provider => "git",
+      source   => "git://github.com/play/play.git",
+      revision => "v3",
+      require => User["${name}"],
+    }
+    file { "play.yml":
+      path    => "/home/${name}/play/config/play.yml",
+      owner   => "${name}",
+      group   => "${name}",
+      mode    => "0644",
+      require => Vcsrepo["/home/${name}/play"],
+      content => template("play/play.yml.erb"),
+    }
+    file { "mpd.conf":
+      path    => "/home/${name}/play/config/mpd.conf",
+      owner   => "${name}",
+      group   => "${name}",
+      mode    => "0644",
+      require => Vcsrepo["/home/${name}/play"],
+      content => template("play/mpd.conf.erb"),
+    }
+    rbenv::install { "${name}":
+      home    => "/home/${name}",
+      require => User["${name}"],
+    }
+    rbenv::compile { "2.0.0-p195":
+      user   => "${name}",
+      home   => "/home/${name}",
+      global => true,
+    }
+  }
+
+  play_user { "muzak":
+    require => Package["git"],
+  }
+}
